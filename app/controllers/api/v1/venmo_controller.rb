aaa53fb12541b96db1da08a299193fec6a0481ec
Harrison Chen <harrisonxchen@gmail.com>
2015-03-01 03:32:35 -0800

    venmo group charge

--- app/controllers/api/v1/venmo_controller.rb
+++ app/controllers/api/v1/venmo_controller.rb
@@ -12,6 +12,23 @@ module Api
         render json: response
       end
 
+      def pay
+        url = "https://api.venmo.com/v1/payments"
+
+        body = {
+          access_token: current_user.auth_token,
+          user_id: params["paymate_id"],
+          note: params["note"],
+          amount: params["amount"],
+          audience: params["audience"]
+        }
+
+        request = Typhoeus.post(url, body: body)
+        response = JSON.parse(request.response_body)
+
+        render json: response
+      end
+
       def charge
         url = "https://api.venmo.com/v1/payments"
 
@@ -29,21 +46,32 @@ module Api
         render json: response
       end
 
-      def pay
+      def charge_group
         url = "https://api.venmo.com/v1/payments"
 
-        body = {
-          access_token: current_user.auth_token,
-          user_id: params["paymate_id"],
-          note: params["note"],
-          amount: params["amount"],
-          audience: params["audience"]
-        }
+        @group = Group.find_by_id(params["group_id"])
 
-        request = Typhoeus.post(url, body: body)
-        response = JSON.parse(request.response_body)
+        amount_total = params["amount"]
+        amount_divided = (amount_total * 1.0 / @group.users.count).ceil
 
-        render json: response
+        response_body = { charges: [] }
+
+        for paymate in @group.users
+          body = {
+            access_token: current_user.auth_token,
+            user_id: paymate.venmo_id,
+            note: params["note"],
+            amount: "-" + amount_divided,
+            audience: params["audience"]
+          }
+
+          request = Typhoeus.post(url, body: body)
+          response = JSON.parse(request.response_body)
+
+          response_body["charges"].push(response);
+        end
+
+        render json: response_body
       end
     end
   end
