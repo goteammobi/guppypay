e82163713e2652011eb8e689653a75819fd71c28
Harrison Chen <harrisonxchen@gmail.com>
2015-03-01 08:07:09 -0800

    awesome group container

--- app/assets/javascripts/guppypay/directives/groups/groups.js
+++ app/assets/javascripts/guppypay/directives/groups/groups.js
@@ -6,6 +6,9 @@ angular.module('directives.groups', [])
     templateUrl: 'assets/guppypay/directives/groups/groups.html',
     controller: function($scope, $element) {
       $scope.friends = []
+      $scope.groups = []
+      $scope.friendsInGroup = [];
+      $scope.showFriends = false;
 
       VenmoService.getFriends()
       .then(function(response) {
@@ -19,6 +22,20 @@ angular.module('directives.groups', [])
           console.log(response);
         })
       };
+
+      $scope.addFriendToGroup = function(user) {
+        $scope.friendsInGroup.unshift(user);
+        var index = $scope.friends.indexOf(user);
+        $scope.friends.splice(index, 1);
+      }
+
+      $scope.test = function() {
+        $scope.showFriends = !$scope.showFriends;
+      }
+
+      $scope.hideFriends = function() {
+        $scope.showFriends = false;
+      }      
     },
     link: function(scope, element, attrs) {
 
